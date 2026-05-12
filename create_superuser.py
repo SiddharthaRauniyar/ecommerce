abc3ccb60ae185a25d1d73303e4f9f3a06c13d63
Siddhartha Rauniyar <siddhartharauniyar5@gmail.com>
2026-05-12 18:14:47 +0545

    Added Missing files

---
+++ create_superuser.py
@@ -0,0 +1,14 @@
+from django.contrib.auth import get_user_model
+import os
+
+User = get_user_model()
+
+username = os.getenv("DJANGO_SUPERUSER_USERNAME")
+email = os.getenv("DJANGO_SUPERUSER_EMAIL")
+password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
+
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
+    print("Superuser created.")
+else:
+    print("Superuser already exists.")
